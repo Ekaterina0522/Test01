abfed50047bb598f004c1961cd0517b24cd12cdb
e.mayantseva <mayantseva>
2021-08-02 13:49:39 +0300

    Используется для написания нового файла TASK_1

--- task1.js
+++ task1.js
@@ -26,77 +26,111 @@ async function getPath() {
 
 }
 
-getPath();	
-///
+getPath();
+// let strPath = String(path);
+// let strPath = '' + path;	
+// console.log( strPath );
+//
 
 
 
 
 // Получаем имена источников в заданной директории
 
-// fs.readdir(soursePath, (err, files) => {
+fs.readdir(path, (err, files) => {
 
-//   if (err)
-//     console.log(err);
+    if (err)
+        console.log(err);
 
-//   else {
-//     console.log("\nНазвания источников:");
-//     files.forEach(file => {
-//       console.log(file);
-//     })
-//   }
-// })
+    else {
+        console.log("\nНазвания источников:");
+        files.forEach(file => {
+            console.log(file);
+        })
+    }
+})
 
 
 
 // получает имена файлов с заданным расширением !!!
-    function getFileName() {
+function getFileName() {
 
-	//делаем из path строку для readdir
-	let strPath = String( path );
+    //делаем из path строку для readdir
+    //let strPath = String( path );
 
-	fs.readdir(strPath, (err, files) => {
+    fs.readdir(path, (err, files) => {
 
-  		if (err)
-    		console.log(err);
+        if (err)
+            console.log(err);
 
-  		else {
-    		console.log("\nFilenames with the .mov extension:");
-    		files.forEach(fileName => {
-    			//смотрим чтобы расширение было mov
-      			if (path.extname(fileName) == ".mov")
-        			return fileName;
-    		})
-  		}
-	})
+        else {
+            console.log("\nFilenames with the .mov extension:");
+            files.forEach(fileName => {
+                //смотрим чтобы расширение было mov
+                if (path.extname(fileName) == ".mov") {
+                    console.log(fileName)
+                    return fileName;
+                }
+            })
+        }
+    })
 }
 
-//getFileName();
+getFileName();
 
-let EpisodeName = '';
-let SequenceName = '';
-let SceneName = '';
-let SceneSubName = '';
+// let EpisodeName = '';
+// let SequenceName = '';
+// let SceneName = '';
+// let SceneSubName = '';
+///
+
+// function parsing( fileName ) {
+
+// 	counter = 0;
+// 	let helperString = '';
+
+// 	for ( let chars of fileName ) {
+
+// 		if ( chars === '_' ) {
+
+// 			EpisodeName += chars;
+// 			counter += 1;
+// 		}
+// 	}
+
+// }
 
 
-function parsing( fileName ) {
 
-	counter = 0;
-	let helperString = '';
 
-	for ( let chars of fileName ) {
-
-		if ( chars === '_' ) {
-
-			EpisodeName += chars;
-			counter += 1;
-		}
-	}
-
+///
+parseString = (path) => {
+    let array = path.split('_'),
+        lastElement;
+    lastElement = array.pop().split(';');
+    lastElement.forEach(e => {
+        array.push(e)
+    })
+    return array;
 }
 
+textToArray = (path, format) => {
+    let err = false; //флаг проверяет количество переменных в формате и тексте
+    let textJSON = '{';
+    let arrayFormat = parseString(format),
+        arrayText = parseString(path);
+    if (arrayFormat.length === arrayText.length) {
+        for (let i = 0; i < arrayFormat.length; i++) {
+            if (i) { textJSON += ',' }
+            textJSON += "\"" + arrayFormat[i].substr(1) + "\":" + "\"" +
+                arrayText[i] + "\""
+        }
+    } else(err = true)
+    textJSON += "}"
+    return err ? 'Ошибка формата' : JSON.parse(textJSON);
+}
 
-
+parseString(path);
 
 
 
