15c35bf0c8c797c716b5c54d1dea86a92ec449cd
e.mayantseva <mayantseva>
2021-08-04 13:59:24 +0300

    Изменена функция getNameObject, она возвращает разбитые на части имена источников

--- Generator.js
+++ Generator.js
@@ -11,23 +11,26 @@ module.exports = class Generator { //extends TASK_1 {
 	}
 
 	//функция генерирующая имена для названия папок
-	static async nameGenerator(splitEntries) {
+	static getNameObject(nameParts) {
+		
+
 		//объект в котором хранятся данные об episodeName,sequenceName,sceneName
-		let name1 = {
-			episodeName: splitEntries[0][0],
-			sequenceName: splitEntries[0][1],
-			sceneName: splitEntries[0][2],
+		let nameObject = {
+			episodeName: nameParts[0],
+			sequenceName: nameParts[1],
+			sceneName: nameParts[2],
+			episodeNumber: undefined,
+			sequenceNumber: undefined,
+			sceneNumber: undefined,
+			sceneSubName: undefined,
 		};
-		console.log(chalk.yellow('episodeName: ', name1.episodeName));
-
-		let episodeName_sequenceName = name1.episodeName+'_'+name1.sequenceName;
-		console.log(chalk.yellow('episodeName_sequenceName: ', episodeName_sequenceName));
-
-		let episodeName_sequenceName_sceneName = name1.episodeName+'_'+name1.sequenceName+'_'+name1.sceneName;
-		console.log(chalk.yellow('episodeName_sequenceName_sceneName: ', episodeName_sequenceName_sceneName));
-
-		
-		
+		// console.log(chalk.yellow('episodeName: ', nameObject.episodeName));
+		nameObject.episodeFullName = nameObject.episodeName+'_'+nameObject.sequenceName;
+		// console.log(chalk.yellow('episodeName_sequenceName: ', episodeName_sequenceName));
+		nameObject.sequenceFullName =  nameObject.episodeName+'_'+nameObject.sequenceName+'_'+nameObject.sceneName;
+		// console.log(chalk.yellow('episodeName_sequenceName_sceneName: ', episodeName_sequenceName_sceneName));
+		// console.log(JSON.stringify(nameObject,true,'  '));
+		return nameObject;
 
 	}
 
@@ -35,15 +38,15 @@ module.exports = class Generator { //extends TASK_1 {
 
 
 
-	//генерируем файловую структуру 
-	static async structureGenerator(path) {
-		// console.log('generationStructure: ',path+'\\3_anim\\subfolder')
-	    fs.mkdir(path + '\\3_anim\\subfolder', { recursive: true }, (err) => {
-	        if (err) throw err;
-	        console.log(`Created in "${path}"`);
-	    });
+	//генерируем файловую структуру (до конца не реализовано)
+	// static async structureGenerator(path) {
+	// 	// console.log('generationStructure: ',path+'\\3_anim\\subfolder')
+	//     fs.mkdir(path + '\\3_anim\\subfolder', { recursive: true }, (err) => {
+	//         if (err) throw err;
+	//         console.log(`Created in "${path}"`);
+	//     });
 
-	}
+	// }
 
 
 
