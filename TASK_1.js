ace4d5ef6307b1068407a0bae3ddd21159e1bca5
e.mayantseva <mayantseva>
2021-08-04 16:47:11 +0300

    Скорректирована работа почти всех функций

--- TASK_1.js
+++ TASK_1.js
@@ -2,14 +2,14 @@ const fs = require('fs-extra');
 const chalk = require('chalk');
 const FileSystem = require('./FileSystem');
 const path = require('path')
-const Generator = require('./Generator');
+const GeneratorOfStructure = require('./GeneratorOfStructure');
 
 //
 class Task1 {
 
-	sourcePath;
-	destPath;
-	
+    sourcePath;
+    destPath;
+
     async start() {
 
         // console.log('@1');
@@ -19,27 +19,28 @@ class Task1 {
         // console.log('@2');
 
         const entries = await this.getDirEntries(this.sourcePath);
-        const arrWithoutLetter = await this.getSimilarStructure(entries);
+
+        const arrWithoutLetter = this.getSimilarStructure(entries);
+
         let splitEntries = await this.parseString(arrWithoutLetter);
         //Заменить в парсинге параметр, после тогго как приведу
         //имена источников к одному виду, и переместить вызов ниже
         //функции getSimilarStructure.
-        
-        await this.getSimilarStructure (entries);
+
+        // this.getSimilarStructure (entries);
 
         await this.parseString(arrWithoutLetter);
 
         //await this.generationStructure(this.destPath);
 
-        splitEntries = splitEntries.map( entry => {
-            return Generator.getNameObject(entry);
+        splitEntries = splitEntries.map(entry => {
+            return GeneratorOfStructure.getNameObject(entry);
         });
 
-        console.log(JSON.stringify(splitEntries,true,'  '));
+        console.log(JSON.stringify(splitEntries, true, '  '));
 
         // console.log('@3');
     }
-
 
     // ввод пользователем пути
     async getPath() {
@@ -54,9 +55,7 @@ class Task1 {
         this.destPath = await FileSystem.validateParam(process.argv[3], "Enter Destination Path");
         if (!this.destPath) return;
         console.log(chalk.bgMagenta('FINSH'));
-
     }
-
 
     // Получаем имена источников в заданной директории
     async getDirEntries(path) {
@@ -65,13 +64,11 @@ class Task1 {
         return entries;
     }
 
-
-    //делаем у всех источников одинаковую структуру
-    async getSimilarStructure(entries) {
+    //делаем у всех источников одинаковое название
+    getSimilarStructure(entries) {
         let joined = [];
-        const arrWithoutLetter = [];
-        entries.forEach(e => {
-            
+        const arrWithoutLetter = entries.map(e => {
+
             let cutName = e.slice(0, -4);
             //во всех источниках ищем sq0_A_0 и заменяем на sq000
             //replaced - источники с замененным sq
@@ -82,13 +79,15 @@ class Task1 {
 
             //замена А на 1 с помощью регулярного выражения
 
-            let replacedWithoutLetter = replaced.replace(/A/g, '1');
+            let replacedWithoutLetter = replaced.replace(/A/g, '1').split('_');
+
             //console.log(chalk.green(replacedWithoutLetter));
 
             //массив с именами источников без букв в конце
-            arrWithoutLetter.push(replacedWithoutLetter);
 
-            });
+            return replacedWithoutLetter;
+
+        });
 
         console.log(arrWithoutLetter);
 
@@ -97,43 +96,23 @@ class Task1 {
     }
 
     //парсим имена источников
-    async parseString (arrWithoutLetter) {
-    	
-    	const splitEntries = [];
-        arrWithoutLetter.forEach( e => {
-    		let eNew = e.split('_')
-    		splitEntries.push(eNew);
-    		
-    	}); 
+    async parseString(arrWithoutLetter) {
+
+        const splitEntries = [];
+        arrWithoutLetter.forEach(e => {
+
+            splitEntries.push(e);
+
+        });
 
         console.log(splitEntries);
-    	return splitEntries;
+        return splitEntries;
     }
-
-    
-
 }
 
-
 //передача аргументов командной строки
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
+process.argv.forEach(function(val, index, array) {
+    console.log(index + ': ' + val);
 });
 
 (new Task1).start();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
