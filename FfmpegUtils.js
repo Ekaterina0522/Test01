465299d37edcba60e93a96cf1d89de585ecbb9dd
e.mayantseva <mayantseva>
2021-08-09 11:50:48 +0300

    улучшила но не работает

--- FfmpegUtils.js
+++ FfmpegUtils.js
@@ -1,12 +1,17 @@
 const pathToFfmpeg = 'c:\\ffmpeg\\bin\\ffmpeg';
 
-module.exports = class FfmpegUtils {
-    let dest1 = NameGeneratornameObject.episodeName;
-    let dest2 = NameGeneratornameObject.sequenceName;
-    let dest3 = NameGeneratornameObject.sceneName;
-    const src_lst = [];
 
+const src_lst = [];
+module.exports = class FfmpegUtils {
+
+
+
+	// получаем имена источников которые переведем в другой формат, извлечем аудио и тд
     async getSrc() {
+
+        let dest1 = NameGeneratornameObject.episodeName;
+        let dest2 = NameGeneratornameObject.sequenceName;
+        let dest3 = NameGeneratornameObject.sceneName;
         for (let e in TASK_1.entries) {
             let src = `D:\\WORK\\Katya\\task1\\mats\\export\\${entries}`;
             src_lst.push(e);
@@ -14,7 +19,7 @@ module.exports = class FfmpegUtils {
         }
     }
 
-
+    // конвертируем в другой формат
     async creatingMP4() {
         const dest = `D:\\WORK\\Katya\\task1\\mats\\import\\3_anim\\${dest1}\\${dest2}\\${dest3}\\cut\\${nameObject.sequenceFullName}.mp4`;
         for (let file in src_lst) {
@@ -22,8 +27,9 @@ module.exports = class FfmpegUtils {
         }
         return;
     }
-
-    async creatingMP3() {
+    
+    // извлекаем аудио
+    async extractingMP3() {
         const dest = `D:\\WORK\\Katya\\task1\\mats\\import\\3_anim\\${dest1}\\${dest2}\\${dest3}\\cut\\${nameObject.sequenceFullName}.mp3`;
         for (let file in src_lst) {
             const { stdout, stderr } = await exec(`c:\\ffmpeg\\bin\\ffmpeg -i ${src} -r 60 -s hd720 ${dest}`);
@@ -31,7 +37,8 @@ module.exports = class FfmpegUtils {
         return;
     }
 
-    async creatingFrame() {
+    //извлекаем кадр
+    async extractingFrame() {
         const dest = `D:\\WORK\\Katya\\task1\\mats\\import\\3_anim\\${dest1}\\${dest2}\\${dest3}\\cut\\${nameObject.sequenceFullName}.jpeg`;
         for (let file in src_lst) {
             const { stdout, stderr } = await exec(`c:\\ffmpeg\\bin\\ffmpeg -i ${src} -r 60 -s hd720 ${dest}`);
