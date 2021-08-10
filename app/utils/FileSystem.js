ac8dbf6ee54279e0d24bee192d7d009a12820138
e.mayantseva <mayantseva>
2021-08-10 12:02:08 +0300

    переместила в другие папки

---
+++ app/utils/FileSystem.js
@@ -0,0 +1,36 @@
+const fs = require('fs-extra');
+const Readline = require('./Readline');
+const chalk = require('chalk');
+const ffmpeg = require('ffmpeg');
+
+//const FfmpegCommand = require(' fluent-ffmpeg ');
+
+//проверяем валидность пути
+module.exports = class FileSystem {
+
+    static async validateParam(path, promptTitle) {
+
+        if (!path) {
+            path = await Readline.readLineAsync(promptTitle);
+        }
+
+        try {
+            if (fs.statSync(path).isDirectory()) {
+                console.log(`Path "${path}" is valid! `);
+                return path;
+            }
+
+        } catch (err) {
+            console.log(`Path "${path}" is not valid! `);
+            return;
+        }
+    }
+
+    //создание папки в заданной директории
+    //директория задается в файле TASK_1, функция start()
+    static async createFolder(path) {
+        console.log('createFolder: ', path);
+        const result = await fs.mkdir(path, { recursive: true });
+        return result;
+    }
+}
---
+++ app/utils/Readline.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const chalk = require('chalk');
+
+
+module.exports = class Readline {
+
+    static async readLineAsync(message) {
+
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        return new Promise((resolve, reject) => {
+            rl.question(message + ': ', (answer) => {
+                resolve(answer);
+            });
+        });
+    }
+
+}
