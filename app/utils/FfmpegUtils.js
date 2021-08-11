5a024f9a890f9bbd2a10f72dd8d59c2ffdb24b91
e.mayantseva <mayantseva>
2021-08-11 14:17:42 +0300

    Работает конвертация, извлечение звука и кадра

--- app/utils/FfmpegUtils.js
+++ app/utils/FfmpegUtils.js
@@ -3,7 +3,6 @@ const child_process = require("child_process");
 const util = require('util');
 const exec = util.promisify(child_process.exec);
 
-const src_lst = [];
 module.exports = class FfmpegUtils {
 
     // изменение размера
@@ -14,18 +13,27 @@ module.exports = class FfmpegUtils {
 
     //конвертация в mp4
     static async convertingToMP4( src, dest ){
-        return { stdout, stderr } = await exec(`c:\\ffmpeg\\bin\\ffmpeg -i ${src} -r 60 -s hd720 ${dest}.mp4`);
+        try{
+            await exec(`c:\\ffmpeg\\bin\\ffmpeg -i ${src} -r 60 -s hd720 -y ${dest}.mp4`);
+        }catch(e) {console.log('convertingToMP4.Error:',e);}
     }
 
 
-    // извлекаем аудио
+    // извлекаем аудио 192 изменяемым сделать, WAV!!!
     static async extractingMP3( src, dest ) {
-            return { stdout, stderr } = await exec(`c:\\ffmpeg\\bin\\ffmpeg -i ${src} -vn -ar 44100 -ac 2 -ab 192k -f mp3 ${dest}.mp3`);
+        try{
+            await exec(`c:\\ffmpeg\\bin\\ffmpeg -i ${src} -vn -ar 44100 -ac 2 -ab 192k -f mp3 -y ${dest}.mp3`);
+        }catch(e) {console.log('extractingMP3.Error:',e);}
     }
 
     //извлекаем кадр
     static async extractingFrame( src, dest ) {
-            return { stdout, stderr } = await exec(`c:\\ffmpeg\\bin\\ffmpeg -i ${src} -r 1 -s -f image2 ${dest}.jpeg`);
+        try{
+
+            const { stdout, stderr } = await exec(`c:\\ffmpeg\\bin\\ffmpeg -ss 0 -i "${src}" -vframes 1 -y "${dest}.jpeg"`);
+            console.log('extractingFrame Complete. ');
+            return { stdout, stderr };
+        }catch(e) {console.log('extractingFrame.Error:',e);}
       
     }
 
