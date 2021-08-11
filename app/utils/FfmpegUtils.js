d814ce71ba2b46340143995e9b0fec0562d58f24
e.mayantseva <mayantseva>
2021-08-11 17:09:31 +0300

    Убрала еще немного мусора

--- app/utils/FfmpegUtils.js
+++ app/utils/FfmpegUtils.js
@@ -9,7 +9,7 @@ module.exports = class FfmpegUtils {
     static async convertingToMP4(src, dest) {
         try {
             await exec(`c:\\ffmpeg\\bin\\ffmpeg -i "${src}" -r 60 -s hd720 -y "${dest}.mp4"`);
-            console.log('convertingVideo Complete. ');
+            //console.log('convertingVideo Complete. ');
         } catch (e) { console.log('convertingToMP4.Error:', e); }
     }
 
@@ -17,7 +17,7 @@ module.exports = class FfmpegUtils {
     static async extractingWAV(src, dest, bitrate=192, sample_rate=44100) {
         try {
             await exec(`c:\\ffmpeg\\bin\\ffmpeg -i "${src}" -vn -ar "${sample_rate}" -ac 2 -ab "${bitrate}k" -f wav -y "${dest}.wav"`);
-            console.log('extractingAudio Complete. ');
+            //console.log('extractingAudio Complete. ');
         } catch (e) { console.log('extractingWAV.Error:', e); }
     }
 
@@ -25,7 +25,7 @@ module.exports = class FfmpegUtils {
     static async extractingFrame(src, dest) {
         try {
             const { stdout, stderr } = await exec(`c:\\ffmpeg\\bin\\ffmpeg -ss 0 -i "${src}" -vframes 1 -vf "scale=-1:320" -y "${dest}.jpg"`);
-            console.log('extractingFrame Complete. ');
+            //console.log('extractingFrame Complete. ');
             return { stdout, stderr };
         } catch (e) { console.log('extractingFrame.Error:', e); }
 
