bf42e9f4af13dc21415d0b555b4d985254480892
e.mayantseva <mayantseva>
2021-08-13 17:42:09 +0300

    закоментировала функцию, чтоб потом переписать

--- app/utils/FileSystem.js
+++ app/utils/FileSystem.js
@@ -37,9 +37,12 @@ module.exports = class FileSystem {
     }
     //бежит по папкам
     static async folderScaner(path){
-        let dir1 = fs.readdir(path);
-        for(let dir2 of dir1){
-            fs.readdir(path+dir2);
-        }
+        // let dir1 = fs.readdir(path);
+        // for(let dir2 of dir1){
+        //     fs.readdir(path+dir2);
+        //}
+        
+        //ссылка на возможно работающую функцию
+        //https://coderoad.ru/41462606/%D0%9F%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C-%D0%B2%D1%81%D0%B5-%D1%84%D0%B0%D0%B9%D0%BB%D1%8B-%D1%80%D0%B5%D0%BA%D1%83%D1%80%D1%81%D0%B8%D0%B2%D0%BD%D0%BE-%D0%B2-%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3%D0%B0%D1%85-NodejS
     }
 }
