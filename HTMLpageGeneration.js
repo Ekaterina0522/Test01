7d79d7c309faeca0c278a432573565524844848c
e.mayantseva <mayantseva>
2021-08-13 12:19:23 +0300

    Импорт библиотек и функций из других файлов. Реализовано получение пути к файлам, его валидация, и запрос и получение version number

---
+++ HTMLpageGeneration.js
@@ -0,0 +1,39 @@
+const fs = require('fs-extra');
+const readline = require('readline');
+const path = require('path');
+const chalk = require('chalk');
+const Readline = require('../../task1/src/app/utils/Readline');
+
+
+class PageGenerator { 
+
+    async start() {
+
+        console.log(chalk.bgMagenta('START'));
+        							//валидация
+        const sourcePath = await Readline.validateParam(process.argv[2], "Enter Source Path");
+        //console.log(chalk.bgBlue('sourcePath:', sourcePath));
+        if (!sourcePath) return;
+
+        const versionNumber = await Readline.readLineAsync("Enter version number");
+        console.log(chalk.bgGreen('versionNumber:', versionNumber));
+        
+
+        console.log(chalk.bgMagenta('FINISH'));
+    }
+
+
+
+}
+
+
+
+
+
+
+    //передача аргументов командной строки
+// process.argv.forEach(function(val, index, array) {
+//     console.log(index + ': ' + val);
+// });
+
+(new PageGenerator).start();
