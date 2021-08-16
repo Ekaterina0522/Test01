545c2667d9a615c52b4b79b410f9a81b1cacde6c
e.mayantseva <mayantseva>
2021-08-16 09:16:37 +0300

    + комм

--- app/utils/FileSystem.js
+++ app/utils/FileSystem.js
@@ -41,8 +41,9 @@ module.exports = class FileSystem {
         // for(let dir2 of dir1){
         //     fs.readdir(path+dir2);
         //}
-        
-        //ссылка на возможно работающую функцию
+
+        //ссылки на возможно работающие функции
+        //https://coderoad.ru/17699599/Node-js-%D0%BF%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D1%8C%D1%82%D0%B5-%D1%81%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D1%83%D0%B5%D1%82-%D0%BB%D0%B8-%D1%84%D0%B0%D0%B9%D0%BB
         //https://coderoad.ru/41462606/%D0%9F%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C-%D0%B2%D1%81%D0%B5-%D1%84%D0%B0%D0%B9%D0%BB%D1%8B-%D1%80%D0%B5%D0%BA%D1%83%D1%80%D1%81%D0%B8%D0%B2%D0%BD%D0%BE-%D0%B2-%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3%D0%B0%D1%85-NodejS
     }
 }
