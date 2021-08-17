023b3d5c4071480f537004574c85639b0dddba74
e.mayantseva <mayantseva>
2021-08-17 11:05:47 +0300

    добавлена функция getVideoLength для определения хронометража видео в секундах(не работает)

--- app/utils/FileSystem.js
+++ app/utils/FileSystem.js
@@ -1,57 +1,65 @@
 const fs = require('fs-extra');
 const chalk = require('chalk');
-const ffmpeg = require('ffmpeg');
+const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
 const Utils = require('./utils');
 
 module.exports = class FileSystem {
 
+    //хронометраж видео в секундах
+    static async getVideoLength(path) {
+        ffmpeg.ffprobe(path, function(err, metadata) {
+            console.log('metadata  ',metadata );
+        });
+    }
+
+
     //получаем последнюю версию файла
-    static async getLatestFile( path, fileType ){
+    static async getLatestFile(path, fileType) {
         // const result = [];
         let fileDate;
         let filePath;
         let fileName;
         // console.log('getLatestFile: ', path, fileType );
-        await FileSystem.eachDirEntry( path, async ( entry, i, fullPath )=>{
+        await FileSystem.eachDirEntry(path, async (entry, i, fullPath) => {
             // console.log('FILTERED: ', entry );
             const stats = await fs.stat(fullPath);
             //если fileDate не undefined или время последнего изменения файла больше чем fileDate
-            if( !fileDate || stats.mtime > fileDate ){
+            if (!fileDate || stats.mtime > fileDate) {
                 fileDate = stats.mtime;
                 filePath = fullPath;
                 fileName = entry;
             }
 
-        }, fileType );
+        }, fileType);
 
         return filePath;
     }
 
     // fileType = true > iterate directories only
-    static async eachDirEntry( path, entryAction, fileType ){
+    static async eachDirEntry(path, entryAction, fileType) {
         //получаем имена каждого файла mp4
         let entries = await FileSystem.getDirEntries(path);
 
-        if( typeof fileType === 'string' ){
+        if (typeof fileType === 'string') {
             //разбиваем названия файлов по . берем то что осталось после точки
             //и смотрим совпадает ли с fileType
-            entries = entries.filter( entry =>{
+            entries = entries.filter(entry => {
                 return entry.split('.').pop().match(fileType);
             });
             //если совпадений нет, сбрасываем fileType чтобы не путаться дальше
             fileType = undefined;
         }
         //бежим по всем именам с нужным нам расширением и соединяем имена с их путями
-        await Utils.processArray( entries, async (entry, i) => {
-            const entryPath = path+'\\'+entry;
-            if( fileType ){
+        await Utils.processArray(entries, async (entry, i) => {
+            const entryPath = path + '\\' + entry;
+            if (fileType) {
                 //если указан fileType то получаем полную ссылку на файл 
                 const _fileType = await fs.lstat(entryPath);
                 // если ищем только папки и данная ссылка не папка то выходим
-                if( fileType === true && !_fileType.isDirectory() ) return;
-            }//строка, индекс. путь
-            await entryAction( entry, i, entryPath );
+                if (fileType === true && !_fileType.isDirectory()) return;
+            } //строка, индекс. путь
+            await entryAction(entry, i, entryPath);
         });
     }
 
@@ -71,16 +79,16 @@ module.exports = class FileSystem {
     }
 
 
-    static async loadTextFile( filePath ){
+    static async loadTextFile(filePath) {
         //var fs = require('fs');
-        const content = await fs.readFile( filePath, 'utf8' );
+        const content = await fs.readFile(filePath, 'utf8');
         return content;
     }
 
-    static async saveTextFile(destPath, file ){
+    static async saveTextFile(destPath, file) {
         fs.writeFile(destPath, file);
         console.log('File saved!');
-        
+
     }
-    
+
 }
