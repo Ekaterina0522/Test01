71e18f804f1daee719e916cb8815c6a32bafb078
e.mayantseva <mayantseva>
2021-08-17 14:18:23 +0300

    получили массивы с номерами секвенций и сцен

--- HTMLpageGeneration.js
+++ HTMLpageGeneration.js
@@ -12,6 +12,8 @@ const FfmpegUtils = require('./app/utils/FfmpegUtils');
 
 const videoFileNames = [];
 const videoFilesDurations = [];
+const sequenceNumbers = [];
+const sceneNumbers = [];
 ///
 class PageGenerator {
 
@@ -19,13 +21,13 @@ class PageGenerator {
 
         console.log(chalk.bgMagenta('START'));
         //валидация
-        
+
         const sourcePath = await Readline.validateParam(process.argv[2], "Enter Source Path");
         //console.log(chalk.bgBlue('sourcePath:', sourcePath));
         if (!sourcePath) return;
 
         const versionNumber = await Readline.readLineAsync("Enter version number");
-        console.log(chalk.bgGreen('versionNumber:', versionNumber)); 
+        console.log(chalk.bgGreen('versionNumber:', versionNumber));
 
         await this.getFilesNames(sourcePath);
         //пробегаемся по всем именам файлов и для каждого ищем длительность в секундах
@@ -34,16 +36,16 @@ class PageGenerator {
             const videoDuration = await FfmpegUtils.getVideoLength(videoFileName);
             videoFilesDurations.push(videoDuration);
 
-        
 
 
-         });
+
+        });
         console.log('videoFilesDurations', videoFilesDurations);
         //await FfmpegUtils.getVideoLength(videoFile);
         // sourcePath
-            // each Sequence
-                // each Scene
-                    // cut/*.mp4
+        // each Sequence
+        // each Scene
+        // cut/*.mp4
 
         //console.log(items);
 
@@ -57,9 +59,9 @@ class PageGenerator {
         // await FileSystem.saveTextFile(sourcePath + items, items);
 
 
-        
+
         console.log(chalk.bgMagenta('FINISH'));
-        
+
         process.exit();
 
     }
@@ -73,25 +75,41 @@ class PageGenerator {
         // console.log('>>>', typeof entries, entries, entries[0] );
 
         // Iterate Sequence
-        await FileSystem.eachDirEntry( path, async ( sqEntry, sqI, sqEntryPath )=>{
+        await FileSystem.eachDirEntry(path, async (sqEntry, sqI, sqEntryPath) => {
             // console.log('==>',sqI+')',sqEntry, sqEntryPath );
 
             // Iterate Scenes
-            await FileSystem.eachDirEntry( sqEntryPath, async ( scEntry, scI, scEntryPath )=>{
-                
+            await FileSystem.eachDirEntry(sqEntryPath, async (scEntry, scI, scEntryPath) => {
+
                 //console.log('Scene Folder ==>',scI+')',scEntry, scEntryPath );
+
+                //обрезаем начало названия видеофайла до номера секвенции
+                scEntry = scEntry.substr(scEntry.lastIndexOf("sq") + 2);
+                console.log(scEntry);
+
+                //первые три цифры это номер секвенции
+                let sequenceNumber = scEntry.substr(0, 3)
+                sequenceNumbers.push(sequenceNumber);
+
+                let sceneNumber = scEntry.substr(scEntry.length - 3);
+                sceneNumbers.push(sceneNumber);
+
                 //получаем самый новый файл в каждой папке cut с расширением mp4
-                const videoFile = await FileSystem.getLatestFile( scEntryPath+'\\cut', 'mp4' );
+                const videoFile = await FileSystem.getLatestFile(scEntryPath + '\\cut', 'mp4');
                 //console.log('file: ', videoFile );
+                
                 //записываем каждый videoFile в массив videoFileNames
                 videoFileNames.push(videoFile);
-                //console.log('<<<videoFileNames>>>', videoFileNames)
-            // true так как перебираем только папки, а не файлы (функция eachDirEntry в файле FileSystem)
-            }, true );
 
-        }, true );
-        
-    }
+
+
+                // true так как перебираем только папки, а не файлы (функция eachDirEntry в файле FileSystem)
+            }, true);
+
+        }, true);
+    //     console.log('<<<sequenceNumbers>>>', sequenceNumbers);
+    //     console.log('<<<sceneNumbers>>>', sceneNumbers);
+    // }
 
 
 }
