a16ce8fd39d73ae33086bbfbbd495210bc4af87c
e.mayantseva <mayantseva>
2021-08-20 13:26:05 +0300

    убраны переменные перед классом. убран ненужный цикл в start()

--- HTMLpageGeneration.js
+++ HTMLpageGeneration.js
@@ -10,14 +10,9 @@ const Utils = require('./app/utils/utils');
 const FfmpegUtils = require('./app/utils/FfmpegUtils');
 
 ///
-let absolutePath;
-const items = []; //массив с объектами где хранятся названия полей таблицы
-let versionNumber;
-let projectName;
+
 ///
 class HTMLpageGenerator {
-
-    
 
     async start() {
 
@@ -35,18 +30,18 @@ class HTMLpageGenerator {
 
         //console.log('__dirname', __dirname);
 
-        await this.validateSourсes(sourcePath, versionNumber, projectName);
+        const items = await this.validateSourсes(sourcePath, versionNumber, projectName);
         const templatePath = __dirname + '\\app\\project\\HTMLpageTemplate.tpl';
         const templateConent = await FileSystem.loadTextFile(templatePath);
 
         const template = Handlebars.compile(templateConent);
 
-        const htmlContent = template({ items: items });
-        await Utils.processArray(items, async (item, i) => {
-            
-            await FileSystem.saveTextFile(sourcePath + `\\${items[i].episodeName}_v${versionNumber}.html`, htmlContent);
-            
+        const htmlContent = template({
+            items,
+            title: items[0].episodeName
         });
+
+        await FileSystem.saveTextFile(sourcePath + `\\${items[0].episodeName}_v${versionNumber}.html`, htmlContent);
 
         console.log(chalk.bgMagenta('FINISH'));
 
@@ -54,7 +49,10 @@ class HTMLpageGenerator {
 
     }
 
-    async validateSourсes(path, versionNumber, projectName) {
+    async validateSourсes(path, versionNumber, projectName ) {
+        
+        const items = [];
+
         console.log('projectName', projectName);
         //получаем имена источников
         const entries = await FileSystem.getDirEntries(path);
@@ -74,17 +72,19 @@ class HTMLpageGenerator {
                 if (scFlag === null) return;
 
                 //console.log('Scene Folder ==>',scI+')',scEntry, scEntryPath );
-                const sceneNameObj = await NameGenerator.fromSceneFullName( scEntry, scEntryPath, versionNumber, projectName );
+                const sceneNameObj = await NameGenerator.makeObject( scEntry, scEntryPath, versionNumber, projectName );
                 items.push(sceneNameObj);
                 
-                return items;
             // true так как перебираем только папки, а не файлы (функция eachDirEntry в файле FileSystem)
             }, true);
 
         }, true);
+
+        return items;
+
     }
         
-    }
+}
 
 //передача аргументов командной строки
 // process.argv.forEach(function(val, index, array) {
